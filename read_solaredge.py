00b3c7cabd00b35dc8b601885996a13d42555f5d
irecord <iain.record@googlemail.com>
2022-08-21 19:30:16 +0100

    Actual Solaredge script

--- read_solaredge.py
+++ read_solaredge.py
@@ -1,35 +1,27 @@
 #!/usr/bin/env python3
 
+import time
 import requests
 import json
-
 import solaredge_modbus
 
-if __name__ == "__main__":
-    inverter = solaredge_modbus.Inverter(
-        device="/dev/ttyUSB0",
-        baud=115200,
-        timeout=30,
-        unit=2
-    )
-
+def doWork():
     export = {} # The values that will be sent to emoncms
-
     values = {} # All the values for detailed output
     values = inverter.read_all()
     meters = inverter.meters()
     batteries = inverter.batteries()
-    values["meters"] = {}
-    values["batteries"] = {}
+#    values["meters"] = {}
+#    values["batteries"] = {}
 
     for meter, params in meters.items():
         meter_values = params.read_all()
-        values["meters"][meter] = meter_values
+#        values["meters"][meter] = meter_values
         export[meter.replace("Meter","M") + "_power_ac"] = meter_values["power"] * 10 ** meter_values["power_scale"]
 
     for battery, params in batteries.items():
         battery_values = params.read_all()
-        values["batteries"][battery] = battery_values
+#        values["batteries"][battery] = battery_values
         battery_name = battery.replace("Battery","B")
         export[battery_name + "_power_ac"] = battery_values["instantaneous_power"]
         export[battery_name + "_temp"] = battery_values["average_temperature"]
@@ -45,10 +37,22 @@ if __name__ == "__main__":
     export["I_power_dc"] = values["power_dc"] * 10 ** values["power_dc_scale"]
     export["I_temp"] = values["temperature"] * 10 ** values["temperature_scale"]
 
-    print(json.dumps(export, indent=4))
+#    print(json.dumps(export, indent=4))
 
     r = requests.post("http://localhost/input/post?node=solaredge&apikey=b215641cf41c8ed618172ec7911f566c&data=" + json.dumps(export))
     print(r.status_code)
     print(r.text)
     if r.text != "ok":
         print(r.json())
+
+if __name__ == "__main__":
+    inverter = solaredge_modbus.Inverter(
+        device="/dev/ttyUSB0",
+        baud=115200,
+        timeout=30,
+        unit=2
+    )
+
+    while True:
+        doWork()
+        time.sleep(5)
